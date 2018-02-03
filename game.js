cf2bc12698fdcb915479fce5c144f486cd68ab38
Brendon Soto <bsoto@nhl.com>
2018-02-03 16:15:43 -0500

    Few updates - Added git ignore - Created basic movement for the ball - Updated notes

--- game.js
+++ game.js
@@ -7,15 +7,34 @@ const canvasHeight = canvas.height
 const defaultColor = '#0095DD'
 
 // Ball stuff
-const ballRadius = 20
+const ballRadius = 10
 const ballStartX = canvasWidth / 2
 const ballStartY = canvasHeight / 2
+let ballX = ballStartX
+let ballY = ballStartY
+let horizontalSpeed = 2
+let verticalSpeed = 2
 
 // Drawing functions
-const drawBall = () => {
+const drawBall = (x, y) => {
     ctx.beginPath()
-    ctx.arc(ballStartX, ballStartY, 20, 0, Math.PI * 2)
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2)
     ctx.fillStyle = defaultColor
     ctx.fill()
     ctx.closePath()
 }
+
+const draw = () => {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+
+    drawBall(ballX, ballY)
+
+    ballX += horizontalSpeed
+    ballY += verticalSpeed
+
+    window.requestAnimationFrame(draw)
+}
+
+
+// Main
+draw()

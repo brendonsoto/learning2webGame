4e579019e1d7310eee9f42164dd96875142efe94
Brendon Soto <bsoto@nhl.com>
2018-02-03 16:27:17 -0500

    Realized I forgot to include the index.html file.
    Implemented basic collision detection for the walls

--- game.js
+++ game.js
@@ -1,19 +1,22 @@
 const canvas = document.getElementById('gameCanvas')
 const ctx = canvas.getContext('2d')
 
-// General stuff
-const canvasWidth = canvas.width
-const canvasHeight = canvas.height
-const defaultColor = '#0095DD'
-
 // Ball stuff
 const ballRadius = 10
-const ballStartX = canvasWidth / 2
-const ballStartY = canvasHeight / 2
+const ballStartX = canvas.width / 2
+const ballStartY = canvas.height / 2
 let ballX = ballStartX
 let ballY = ballStartY
 let horizontalSpeed = 2
 let verticalSpeed = 2
+
+// General stuff
+const defaultColor = '#0095DD'
+const upperBoundary = canvas.height - ballRadius
+const lowerBoundary = ballRadius
+const leftBoundary = ballRadius
+const rightBoundary = canvas.width - ballRadius
+
 
 // Drawing functions
 const drawBall = (x, y) => {
@@ -24,10 +27,24 @@ const drawBall = (x, y) => {
     ctx.closePath()
 }
 
+// Collision detections
+// Validate Horizontal/Vertical Direction - checks if direction needs to be reversed
+const validateHorizontalDirection = (x, dx) => {
+    if (x < leftBoundary || x > rightBoundary) { return -dx; }
+    return dx;
+}
+const validateVerticalDirection = (y, dy) => {
+    if (y < lowerBoundary || y > upperBoundary) { return -dy; }
+    return dy;
+}
+
 const draw = () => {
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
 
     drawBall(ballX, ballY)
+
+    horizontalSpeed = validateHorizontalDirection(ballX, horizontalSpeed)
+    verticalSpeed = validateVerticalDirection(ballY, verticalSpeed)
 
     ballX += horizontalSpeed
     ballY += verticalSpeed
